d7d18567517275fb8b0d624cf7595bebf21a39df
Rimsha Shahid <rimshashahid218@gmail.com>
2024-09-17 13:18:14 +0500

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import img from "./img.jpeg";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Rimsha Shahid</h1>
+      <h3>Web developer</h3>
+      <div className="img">
+        {/* <img src={img} className="image" alt="A profile" /> */}
+      </div>
+      <h4>Comming soon...</h4>
+    </div>
+  );
+}
+
+export default App;
